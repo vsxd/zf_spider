a710119d4766e82e1fb45ca0ccae592e637c26fd
xudong714 <xudong714@gmail.com>
2018-08-05 15:46:37 +0800

    separate main from spider.py

--- spider.py
+++ spider.py
@@ -208,31 +208,3 @@ class CourseTableParser:
         for index in range(1, len(table)):
             self.pretty_table.add_row(table[index])
         print(self.pretty_table)
-
-
-def menu():
-    print(' -'*20)
-    print('      方正教务系统爬虫 for AHUT')
-    print(' -'*20)
-    menu_item = {'1': InfoStorage.login,
-                '2': CourseTableParser.start,
-                '3': 2,
-                '4': 3,
-                'q': exit}
-    print('     请输入功能的数字序号以进行后续操作')
-    print('     + 1.登陆用户')
-    print('     + 2.显示课表')
-    print('     + 3.显示考试时间')
-    print('     + 4.抢课')
-    print('     + q.退出程序')
-    print(' -'*20)
-    input_selection = input()
-    menu_item.get(input_selection)()
-
-
-if __name__ == '__main__':
-    # main()
-    InfoStorage.SESSION = requests.session()
-    while True:
-        menu()
-    InfoStorage.SESSION.close()
