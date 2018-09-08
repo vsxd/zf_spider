c83ab8a7763c2677fdaaa23300917e40a70df8a3
xudong714 <xudong714@gmail.com>
2018-09-08 21:43:58 +0800

    catch keyboard interruption

--- main.py
+++ main.py
@@ -13,19 +13,24 @@ def menu():
                 '4': 3,
                 'q': exit}
     print('     请输入功能的数字序号以进行后续操作')
-    print('     + 1.登陆用户')
-    print('     + 2.显示课表')
-    print('     + 3.显示考试时间')
-    print('     + 4.抢课')
-    print('     + q.退出程序')
+    print('     > 1.登陆用户')
+    print('     > 2.显示课表')
+    print('     > 3.显示考试时间')
+    print('     > 4.抢课')
+    print('     > q.退出程序')
     print(' -'*20)
     input_selection = input()
     menu_item.get(input_selection)()
 
 
 if __name__ == '__main__':
-    # main()
-    InfoStorage.SESSION = requests.session()
-    while True:
-        menu()
-    InfoStorage.SESSION.close()
+    try:
+        InfoStorage.SESSION = requests.session()
+        while True:
+            menu()
+        InfoStorage.SESSION.close()
+    except KeyboardInterrupt as e:
+        InfoStorage.SESSION.close()
+        print()
+        print('程序终止  Bye~')
+        exit(0)
